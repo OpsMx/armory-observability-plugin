4ae63c7ea24b607a0816738d0188ca02c90ce953
sudhakaropsmx <sudhakar@opsmx.io>
2025-04-29 09:19:28 +0530

    OP-23416: Bugfix for CVE update spring boot version dependency changes

--- common/src/main/java/io/micrometer/prometheus/MutatedPrometheusMeterRegistry.java
+++ common/src/main/java/io/micrometer/prometheus/MutatedPrometheusMeterRegistry.java
@@ -42,7 +42,7 @@ import java.util.function.ToDoubleFunction;
 import java.util.function.ToLongFunction;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.joining;
+
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.StreamSupport.stream;
 
